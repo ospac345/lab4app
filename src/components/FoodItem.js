df43538c40e35b220b38bc9c4ee789373a298b8e
Syed Faisal Imam <45050035+ospac345@users.noreply.github.com>
2022-10-27 22:45:46 +0100

    Search functionality added

--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -1,13 +1,11 @@
 import React from "react";
-import Accordion from 'react-bootstrap/Accordion';
 const FoodItem = ({ food }) => {
   return (
-    <div>
-      <Accordion.Header>{food.name}</Accordion.Header>
-      <Accordion.Body>
-        <p>{food.description}</p>
-        <p>{food.price}</p>
-      </Accordion.Body>
-</div>
+<div className="card" style={{width: 50+"%"}}> <div className="card-body">
+        <h5 className="card-title">{food.name}</h5>
+        <h6 className="card-subtitle mb-2 text-muted">£{food.price}</h6>
+        <p className="card-text">{food.description}</p>
+      </div>
+    </div>
 ); };
-export default FoodItem;
+export default FoodItem
